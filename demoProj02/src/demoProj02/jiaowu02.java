e7ce9dfd8d5ed7b8c2e27cf0fb6998604f0f8290
Lenovo <Lenovo@DESKTOP-CT0OQLT.mshome.net>
2019-05-23 21:38:14 +0800

    i

---
+++ demoProj02/src/demoProj02/jiaowu02.java
@@ -0,0 +1,8 @@
+package demoProj02;
+
+public class jiaowu02 {
+	public static void main(String[] args) {
+		System.out.println("good!");
+	}
+
+}
---
+++ demoProj02/src/demoProj02/student.java
@@ -0,0 +1,11 @@
+package demoProj02;
+
+public class student {
+	public String name;
+	   public String address;
+	   
+	   student(){
+		   
+	   }
+
+}
